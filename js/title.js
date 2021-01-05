3e6e472db4cd4daa4cec4acbfad6056f693794c8
Frank Alania <frankkeren66@gmail.com>
2021-01-05 16:47:42 -0500

    proyecto

---
+++ js/title.js
@@ -0,0 +1,11 @@
+$(function () {
+    $(".text-typed").typed({
+      strings:["Webet", "Web Developer", "Photoshop", "Coffee Drinker"],
+      typeSpeed: 60,
+      backSpeed: 20,
+      backDelay: 1500,
+      showCursor: true,
+      loop: true,
+      cursorChar: '_',
+    });
+  });
